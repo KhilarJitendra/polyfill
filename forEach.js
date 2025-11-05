3e776cbc6dc07c2bacf93006e49a573f7abb067b
Jitendra Khilar <97145002+KhilarJitendra@users.noreply.github.com>
2025-11-05 21:31:05 +0530

    Update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,1 +1,26 @@
-// Implment for Each
+---------START------->(05-Nov-2025)
+
+forEach - 
+  1. take function as argument
+  2. it does not return anything
+
+Use Case - 
+  const arr = [1, 2, 3, 4]
+  function callback(a) {
+      console.log(a*a);
+  }
+  arr.forEach(callback);
+
+PolyFill -
+
+  Array.prototype.myForEach = function (callback) {
+    for(let i=0; i< this.length; i++) {
+      // current, index, array
+      callback(this[i], i, this);
+    }
+  }
+
+  const newArr = [1, 2, 3, 4]
+  newArr.myForEach(callback);
+
+---------END------->
