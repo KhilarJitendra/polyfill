1d93a044c615d92d4be663b72397865b5695ee7e
Jitendra Khilar <97145002+KhilarJitendra@users.noreply.github.com>
2025-11-08 19:44:41 +0530

    Update flat.js

--- flat.js
+++ flat.js
@@ -1,28 +1,34 @@
----------START------->(08-Nov-2025)
-Flat 
-  1. Take a argument or default 1
-  2. return array
+// ---------START------->(08-Nov-2025)
+// Polyfill for flat()
 
-Use case 
-  const arr = [1, [2, 1, [3, 4]], 4];
-  const result = arr.flat(1);
-  console.log(result) 
+// Steps:
+// 1. Take a depth argument (default = 1)
+// 2. Return a new flattened array up to the specified depth
 
-Polyfill - 
-  Array.prototype.myFlat = function(depth = 1) {
-      let result = [];
-      for(let i=0; i< this.length; i++){
-        const value = this[i]
-        if (Array.isArray(value)) {
-          result = result.concat(value.myFlat(depth - 1))
-        } else {
-          result.push(value)
-        }
-      }
+// Use case
+const arr = [1, [2, 1, [3, 4]], 4];
+const result = arr.flat(1);
+console.log(result); // [1, 2, 1, [3, 4], 4]
 
-     return result;
+// Polyfill
+Array.prototype.myFlat = function (depth = 1) {
+  let result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    const value = this[i];
+
+    if (Array.isArray(value) && depth > 0) {
+      // Recursively flatten one level less
+      result = result.concat(value.myFlat(depth - 1));
+    } else {
+      result.push(value);
+    }
   }
 
-  console.log(arr.myFlat(1));
+  return result;
+};
 
----------END------->
+// Example usage
+console.log(arr.myFlat(1)); // [1, 2, 1, [3, 4], 4]
+
+// ---------END------->
