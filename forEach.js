dfded03e5ad89996e2decabd1d83e88b2ed968f3
Jitendra Khilar <97145002+KhilarJitendra@users.noreply.github.com>
2025-11-08 19:45:09 +0530

    Refactor forEach polyfill and examples

--- forEach.js
+++ forEach.js
@@ -1,26 +1,28 @@
----------START------->(05-Nov-2025)
+// ---------START------->(05-Nov-2025)
+// Polyfill for forEach()
 
-forEach - 
-  1. take function as argument
-  2. it does not return anything
+// Steps:
+// 1. Take a callback function as an argument
+// 2. Execute the callback for each element
+// 3. Does not return anything
 
-Use Case - 
-  const arr = [1, 2, 3, 4]
-  function callback(a) {
-      console.log(a*a);
+// Use Case
+const arr = [1, 2, 3, 4];
+function callback(a) {
+  console.log(a * a);
+}
+arr.forEach(callback); // 1, 4, 9, 16
+
+// Polyfill
+Array.prototype.myForEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    // callback(currentValue, index, array)
+    callback(this[i], i, this);
   }
-  arr.forEach(callback);
+};
 
-PolyFill -
+// Example usage
+const newArr = [1, 2, 3, 4];
+newArr.myForEach(callback); // 1, 4, 9, 16
 
-  Array.prototype.myForEach = function (callback) {
-    for(let i=0; i< this.length; i++) {
-      // current, index, array
-      callback(this[i], i, this);
-    }
-  }
-
-  const newArr = [1, 2, 3, 4]
-  newArr.myForEach(callback);
-
----------END------->
+// ---------END------->
